4e3419dfddde8550e6f01091e6aace15d42a4984
si701@hotmail.co.uk <si701@hotmail.co.uk@c39b785b-39ce-5082-1355-c0f9be8a0ba5>
2011-08-15 12:22:45 +0000

    Version Info - updated

--- workspaces/wigga/kocpowertools@mat.user.js
+++ workspaces/wigga/kocpowertools@mat.user.js
@@ -6,7 +6,7 @@
 // @require        http://tomchapin.me/auto-updater.php?id=103659
 // ==/UserScript==
 
-var Version = '20110804b';
+var Version = '20110815a';
 
 var Title = 'KOC Power Tools';
 var DEBUG_BUTTON = true;
