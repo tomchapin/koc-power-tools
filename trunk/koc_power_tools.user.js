3cec41205decc69835fcdab033ba935312a9f931
baos780@gmail.com <baos780@gmail.com@c39b785b-39ce-5082-1355-c0f9be8a0ba5>
2013-01-21 05:59:15 +0000

    stripped out some upgraded useless code in tower

--- trunk/koc_power_tools.user.js
+++ trunk/koc_power_tools.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name           KOC Power Tools
 // @namespace      mat
-// @version        20130120b
+// @version        20130120c
 // @include        *.kingdomsofcamelot.com/*main_src.php*
 // @description    Enhancements and bug fixes for Kingdoms of Camelot
 // @icon  http://www.gravatar.com/avatar/f9c545f386b902b6fe8ec3c73a62c524?r=PG&s=60&default=identicon
@@ -14,7 +14,7 @@ if(window.self.location != window.top.location){
 	}
 }
 
-var Version = '20130120b';
+var Version = '20130120c';
 
 var Title = 'KOC Power Tools';
 var DEBUG_BUTTON = true;
@@ -751,17 +751,6 @@ var ChatStuff = {
 			element_class = ' ptChatAttack';
 		if (m[0].indexOf('My wilderness at') >= 0 && Options.chatAttack)
 			element_class = ' ptChatAttack';
-		if (m[0].indexOf(unsafeWindow.g_js_strings.modal_messages_viewreports.attackedby) >= 0 && Options.chatAttack) {
-			if(m[0].indexOf(unsafeWindow.g_js_strings.attack_generateincoming.estimatedarrival) >= 0) {
-				element_class = ' ptChatAttack';
-				msg = msg.replace(/\|/g,'<br>');
-			}
-			if(m[0].indexOf(unsafeWindow.g_js_strings.incomingattack.attackrecalled) >= 0){
-				element_class = ' ptChatRecall';
-				msg = msg.replace(/\|/g,'<br>');
-			}
-			
-		}
 		//barcode style catch
 		if (m[0].indexOf('..:.') >= 0 && Options.chatAttack) {
 			element_class = ' ptChatAttack';
@@ -801,16 +790,6 @@ var ChatStuff = {
 			AudioManager.setSource(SOUND_FILES.alert);
 			AudioManager.play();
 			setTimeout(function(){AudioManager.stop();}, 5000);
-		}
-		if (m[0].indexOf(unsafeWindow.g_js_strings.modal_messages_viewreports.attackedby) >= 0 && Options.chatAttack) {
-			if(m[0].indexOf(unsafeWindow.g_js_strings.attack_generateincoming.estimatedarrival) >= 0) {
-			AudioManager.setSource(SOUND_FILES.alert);
-			AudioManager.play();
-			setTimeout(function(){AudioManager.stop();}, 5000);
-			}
-			if(m[0].indexOf(unsafeWindow.g_js_strings.incomingattack.attackrecalled) >= 0)
-				{}//nothing thus far
-			
 		}
 			
 	}
