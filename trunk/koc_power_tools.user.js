4925509da98e4ee3243a31eb31f07a87905c1b9f
phillip.cazzola@gmail.com <phillip.cazzola@gmail.com@c39b785b-39ce-5082-1355-c0f9be8a0ba5>
2013-01-26 20:08:17 +0000

    Bug fix for alliance report monitoring (undefined variable)

--- trunk/koc_power_tools.user.js
+++ trunk/koc_power_tools.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name           KOC Power Tools
 // @namespace      mat
-// @version        20130125d
+// @version        20130126a
 // @include        *.kingdomsofcamelot.com/*main_src.php*
 // @description    Enhancements and bug fixes for Kingdoms of Camelot
 // @icon  http://www.gravatar.com/avatar/f9c545f386b902b6fe8ec3c73a62c524?r=PG&s=60&default=identicon
@@ -14,7 +14,7 @@ if(window.self.location != window.top.location){
 	}
 }
 
-var Version = '20130125d';
+var Version = '20130126a';
 
 var Title = 'KOC Power Tools';
 var DEBUG_BUTTON = true;
@@ -13471,8 +13471,7 @@ postToCell : function (m, playerNames, cityNames){
   fetchreport : function (rpId,rpt,msg,playerNames,cityNames) {
     var t = AllianceReportsCheck;
 	  if(Options.alertmtroops==0) {
-					if(Options.alertmtroops > trooptot)return;
-					 var automsg = sendChat('/a '+trooptot+' '+msg);
+					var automsg = sendChat('/a ' +msg);
 					if(Options.celltext.enable)
 						t.postToCell(rpt, playerNames, cityNames);
 					if(Options.alertConfig.sound)
