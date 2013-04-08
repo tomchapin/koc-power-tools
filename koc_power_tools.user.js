7eeab93ee97da3ef9e7a26023cbab906a6d368e8
baos780@gmail.com <baos780@gmail.com@c39b785b-39ce-5082-1355-c0f9be8a0ba5>
2013-04-08 22:19:33 +0000

    removed market trade limit

--- koc_power_tools.user.js
+++ koc_power_tools.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name           KOC Power Tools
 // @namespace      mat
-// @version        20130401a
+// @version        20130408a
 // @include        *.kingdomsofcamelot.com/*main_src.php*
 // @description    Enhancements and bug fixes for Kingdoms of Camelot
 // @icon  http://www.gravatar.com/avatar/f9c545f386b902b6fe8ec3c73a62c524?r=PG&s=60&default=identicon
@@ -14,7 +14,7 @@ if(window.self.location != window.top.location){
 	}
 }
 
-var Version = '20130401a';
+var Version = '20130408a';
 
 var Title = 'KOC Power Tools';
 var DEBUG_BUTTON = true;
@@ -13991,7 +13991,8 @@ function getRallypoint(cityId){
 	return rallypointlevel;
 }
 
-
+    var Market = new CalterUwFunc ('modal_marketplace', [[/maxlength..\d./gim, '']]);
+    Market.setEnable (true);
 
 
 ptStartup ();
