3471d7760e9a3d56417e0f82fe8a64c0bc2cacc2
idaneaux@gmail.com <idaneaux@gmail.com@c39b785b-39ce-5082-1355-c0f9be8a0ba5>
2013-05-17 05:28:13 +0000

    change initial setting for aetherstone display fix to 'false'

--- koc_power_tools.user.js
+++ koc_power_tools.user.js
@@ -1,7 +1,7 @@
 ﻿// ==UserScript==
 // @name           KOC Power Tools
 // @namespace      mat
-// @version        20130516a
+// @version        20130516b
 // @include        *.kingdomsofcamelot.com/*main_src.php*
 // @description    Enhancements and bug fixes for Kingdoms of Camelot
 // @icon  http://www.gravatar.com/avatar/f9c545f386b902b6fe8ec3c73a62c524?r=PG&s=60&default=identicon
@@ -14,7 +14,7 @@ if(window.self.location != window.top.location){
 	}
 }
 
-var Version = '20130516a';
+var Version = '20130516b';
 
 var Title = 'KOC Power Tools';
 var DEBUG_BUTTON = true;
@@ -151,7 +151,7 @@ var Options = {
   mapInfo2: false,
   mapInfo3: false,
   fixApothTime: true,
-  fixTRAetherCost: true,
+  fixTRAetherCost: false,
   multiBrowserAllow : false
 };
 
