ba03e3cf6332ab65ba3bdc61f6b12622b65f6fcb
pcwong13 <pcwong13@c39b785b-39ce-5082-1355-c0f9be8a0ba5>
2013-08-18 04:04:45 +0000

    Bug fix Tools->Report When a report has astone resource, the report fails to open.

--- trunk/koc_power_tools.user.js
+++ trunk/koc_power_tools.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name           KOC Power Tools
 // @namespace      mat
-// @version        20130810a
+// @version        20130817
 // @include        *.kingdomsofcamelot.com/*main_src.php*
 // @description    Enhancements and bug fixes for Kingdoms of Camelot
 // @icon  http://www.gravatar.com/avatar/f9c545f386b902b6fe8ec3c73a62c524?r=PG&s=60&default=identicon
@@ -14,7 +14,7 @@ if(window.self.location != window.top.location){
 	}
 }
 
-var Version = '20130810a';
+var Version = '20130817';
 
 var Title = 'KOC Power Tools';
 var DEBUG_BUTTON = true;
@@ -10278,6 +10278,7 @@ Tabs.Rpt = {
 		woodImg = '<img src=http://cdn1.kingdomsofcamelot.com/fb/e2/src/img/wood_30.png></TD><TD>Wood';
 		stoneImg = '<img src=http://cdn1.kingdomsofcamelot.com/fb/e2/src/img/stone_30.png></TD><TD>Stone';
 		oreImg = '<img src=http://cdn1.kingdomsofcamelot.com/fb/e2/src/img/iron_30.png></TD><TD>Ore';
+		astoneImg = '<img src=http://cdn1.kingdomsofcamelot.com/fb/e2/src/img/aetherstone_30.png></TD><TD>Aetherstone';
 
 		function buildHeader () {
 			var h='<TABLE class=ptTab width=100%>';
