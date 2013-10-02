a8a72c593e2395b0cf3d8bb6e7e7350003f1ca4b
idaneaux@gmail.com <idaneaux@gmail.com@c39b785b-39ce-5082-1355-c0f9be8a0ba5>
2013-10-02 05:04:52 +0000

    Disable march patch - see whether this resolves the raid return time issue.

--- trunk/koc_power_tools.user.js
+++ trunk/koc_power_tools.user.js
@@ -1,7 +1,7 @@
 ﻿// ==UserScript==
 // @name           KOC Power Tools
 // @namespace      mat
-// @version        20130929
+// @version        20131001a
 // @include        *.kingdomsofcamelot.com/*main_src.php*
 // @description    Enhancements and bug fixes for Kingdoms of Camelot
 // @icon  http://www.gravatar.com/avatar/f9c545f386b902b6fe8ec3c73a62c524?r=PG&s=60&default=identicon
@@ -14,7 +14,7 @@ if(window.self.location != window.top.location){
 	}
 }
 
-var Version = '20130929';
+var Version = '20131001a';
 
 var Title = 'KOC Power Tools';
 var DEBUG_BUTTON = true;
@@ -382,7 +382,7 @@ if (TEST_WIDE){
   mmbImageFix.init();
   ChatTimeFix.init();
   bypassMulti.init();
-  BarbRaidMarchPatch.init();
+//  BarbRaidMarchPatch.init();
   towho.init();
   cdtd.init();
   tabManager.init (mainPop.getMainDiv());
