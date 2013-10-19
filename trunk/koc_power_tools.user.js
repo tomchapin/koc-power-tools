90bd4157d1183341856bb50f719ea2ade1978d04
idaneaux@gmail.com <idaneaux@gmail.com@c39b785b-39ce-5082-1355-c0f9be8a0ba5>
2013-10-19 15:51:43 +0000

    Add confirmation check before changing city defend status

--- trunk/koc_power_tools.user.js
+++ trunk/koc_power_tools.user.js
@@ -1,7 +1,7 @@
 ﻿// ==UserScript==
 // @name           KOC Power Tools
 // @namespace      mat
-// @version        20131015a
+// @version        20131019a
 // @include        *.kingdomsofcamelot.com/*main_src.php*
 // @description    Enhancements and bug fixes for Kingdoms of Camelot
 // @icon  http://www.gravatar.com/avatar/f9c545f386b902b6fe8ec3c73a62c524?r=PG&s=60&default=identicon
@@ -14,7 +14,7 @@ if(window.self.location != window.top.location){
 	}
 }
 
-var Version = '20131015a';
+var Version = '20131019a';
 
 var Title = 'KOC Power Tools';
 var DEBUG_BUTTON = true;
@@ -15337,8 +15337,11 @@ var cdtd = {
 			method: "post",
 			parameters: params,
 			onSuccess: function (message) {
+	            	    var rslt = eval("(" + message.responseText + ")");
+		    	    if (rslt.ok) {
 				uW.seed.citystats["city" + city].gate = state;
 				t.drawdefendstatus();
+			    }
 			},
 			onFailure: function () {
 				t.drawdefendstatus();
