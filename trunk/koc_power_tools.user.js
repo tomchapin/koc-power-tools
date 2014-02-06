bae9f565a4e1b256fdb4455c76f88fd8c8ececeb
barbarossa69@gmail.com <barbarossa69@gmail.com@c39b785b-39ce-5082-1355-c0f9be8a0ba5>
2014-02-06 14:22:11 +0000

    typo

--- trunk/koc_power_tools.user.js
+++ trunk/koc_power_tools.user.js
@@ -1,7 +1,7 @@
 ﻿// ==UserScript==
 // @name           KOC Power Tools
 // @namespace      mat
-// @version        20140206a
+// @version        20140206b
 // @include        *.kingdomsofcamelot.com/*main_src.php*
 // @description    Enhancements and bug fixes for Kingdoms of Camelot
 // @icon  http://www.gravatar.com/avatar/f9c545f386b902b6fe8ec3c73a62c524?r=PG&s=60&default=identicon
@@ -16,9 +16,9 @@ if(window.self.location != window.top.location){
 
 //This value is used for statistics (https://nicodebelder.eu/kocReportView/Stats.html).
 //Please change it to your Userscript project name.
-var SourceName = "KOC Power Tools (KOCScripters)";
+var SourceName = "KOC Power Tools (SVN)";
 
-var Version = '20140206a';
+var Version = '20140206b';
 var Title = 'KOC Power Tools';
 var DEBUG_BUTTON = true;
 var DEBUG_TRACE = false;
