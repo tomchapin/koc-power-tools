b06b08f69da689ccf7ac89d4cb8b41e2b6e87f74
barbarossa69@gmail.com <barbarossa69@gmail.com@c39b785b-39ce-5082-1355-c0f9be8a0ba5>
2014-03-14 09:13:04 +0000

    Typo (advisor, not adviser!)

--- koc_power_tools.user.js
+++ koc_power_tools.user.js
@@ -309,7 +309,7 @@ function ptStartup (){
   var trstyles = 'div#throneMainContainer div#tableContainer{width:80px;height:213px;top:400px;left:450px;}\
 				div#throneMainContainer div#trophyContainer{width:71px;height:86px;top:41px;left:381px;z-index:97;}\
 				div#throneMainContainer div#statueContainer{width:124px;height:296px;top:274px;left:150px;z-index:99;}\
-				div#throneMainContainer div#adviserContainer{width:191px;height:240px;bottom:0pt;right:-30pt;}\
+				div#throneMainContainer div#advisorContainer{width:141px;height:240px;bottom:0pt;right:0pt;}\
 				div#throneMainContainer div#heroContainer{width:85px;height:136px;top:173px;left:450px;z-index:99;}';
   
   var styles = '\
