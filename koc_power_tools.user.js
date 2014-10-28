f21a3a7e420b32a891bcca1f39744e82ddf92f66
barbarossa69@gmail.com <barbarossa69@gmail.com@c39b785b-39ce-5082-1355-c0f9be8a0ba5>
2014-10-28 11:13:04 +0000

    Fix to autobuild defences (parseInt instead of Math.round) thanks Audrius

--- koc_power_tools.user.js
+++ koc_power_tools.user.js
@@ -1,7 +1,7 @@
 ﻿// ==UserScript==
 // @name           KOC Power Tools
 // @namespace      mat
-// @version        20141028a
+// @version        20141028b
 // @include        *.kingdomsofcamelot.com/*main_src.php*
 // @description    Enhancements and bug fixes for Kingdoms of Camelot
 // @icon  http://www.gravatar.com/avatar/f9c545f386b902b6fe8ec3c73a62c524?r=PG&s=60&default=identicon
@@ -25,7 +25,7 @@ if (window.self.location != window.top.location) {
 //This value is used for statistics (https://nicodebelder.eu/kocReportView/Stats.html).
 //Please change it to your Userscript project name.
 var SourceName = "KOC Power Tools (SVN)";
-var Version = '20141028a';
+var Version = '20141028b';
 var Title = 'KOC Power Tools';
 var DEBUG_BUTTON = true;
 var DEBUG_TRACE = false;
@@ -8689,7 +8689,7 @@ function getWallInfo(cityId, objOut) {
 	for (var i = 1; i < (objOut.wallLevel + 1); i++)
 		spots += (i * 1500);
 	if (unsafeWindow.seed.cityData.city[cityId].isPrestigeCity) {
-		if (unsafeWindow.seed.cityData.city[cityId].prestigeInfo.blessings.indexOf(307) != -1) spots = Math.round(spots * 1.15);		
+		if (unsafeWindow.seed.cityData.city[cityId].prestigeInfo.blessings.indexOf(307) != -1) spots = parseInt(spots * 1.15);		
 	}	
 	objOut.wallSpace = spots;
 	objOut.fieldSpace = spots;
